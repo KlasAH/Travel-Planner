2c407d8ad15351d7bd9d438ef985a4df51c06e86
KlasAH <88081488+KlasAH@users.noreply.github.com>
2025-12-30 01:07:03 +0100

    feat(config): Update Vite configuration
    Configure Vite to load environment variables, set the development server port to 3000, and enable alias for path resolution. This improves the development experience and prepares for environment-specific configurations.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'app.tp.klas.ahlnet.nu',
-      'localhost',
-      '127.0.0.1'
-    ]
-  }
-})
+// __dirname is not available in ES modules. This defines it using import.meta.url.
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+        allowedHosts: true,
+      },
+      plugins: [react()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './'),
+        },
+      },
+    };
+});
